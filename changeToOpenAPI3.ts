972cb1b813fa582e8ca5bb76b25baf64c328e36c
kobatastu <t.kobayashi107@gmail.com>
2020-02-15 17:02:52 +0900

    remove buildV3

--- changeToOpenAPI3.ts
+++ changeToOpenAPI3.ts
@@ -2,8 +2,6 @@ import { parse } from 'swagger-parser'
 import { OpenAPI, OpenAPIV3 } from 'openapi-types'
 import * as fs from 'fs'
 
-import buildV3 from './buildV3'
-
 export interface Template {
   baseURL: string
   types?: string
@@ -13,18 +11,14 @@ export interface Template {
   }[]
 }
 
-const targetJson = 'result_500.json'
-
 const isV3 = (openapi: OpenAPI.Document): openapi is OpenAPIV3.Document => 'openapi' in openapi
 
 const changeFunc = async (input: string | OpenAPI.Document, isYaml: boolean): Promise<Template> => {
   const openapi = await parse(input, { parse: { json: !isYaml } })
-  console.log(openapi)
-
-  return buildV3(
-    isV3(openapi) ? openapi : await require('swagger2openapi').convertObj(openapi, { direct: true })
-  )
+  return isV3(openapi) ? openapi : await require('swagger2openapi').convertObj(openapi, { direct: true })
 }
+
+const targetJson = 'result_500.json'
 
 const originalResultJson = JSON.parse(`${fs.readFileSync(targetJson,'utf8')}`)
 const resultJson = originalResultJson.data
