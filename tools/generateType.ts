53f133a825f01b96cc7da565a4fd226a5c57296a
kobatastu <t.kobayashi107@gmail.com>
2020-07-19 11:00:44 +0900

    modify generateType

--- tools/generateType.ts
+++ tools/generateType.ts
@@ -1,8 +1,15 @@
 import buildTemplate from "openapi2aspida/dist/buildTemplate";
 import * as fs from "fs";
 
-const targetJson;
+const targetJson = "sources/sample.json";
 const originalResultJson = JSON.parse(`${fs.readFileSync(targetJson, "utf8")}`);
-const resultJson = originalResultJson.data;
+const resultJson = originalResultJson.data[0];
 
-buildTemplate(resultJson, false, false, false);
+const generateType = async () => {
+  const newResultJson = await buildTemplate(resultJson, false, false, false);
+  fs.writeFile(`result.json`, JSON.stringify(newResultJson), (error) => {
+    if (error) console.log("Error", error);
+  });
+};
+
+generateType();
